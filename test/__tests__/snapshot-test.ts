ab6803d3d9545d4ec5168241b8ffa30f4d5829e5
Himenon <k.himeno314@gmail.com>
2021-01-21 14:58:28 +0900

    test: update

--- test/__tests__/snapshot-test.ts
+++ test/__tests__/snapshot-test.ts
@@ -3,11 +3,11 @@ import * as path from "path";
 
 describe("Generate Code Snapshot Test", () => {
   test("OnlyLocalRef.ts", () => {
-    const code = fs.readFileSync(path.join(__dirname, "../OnlyLocalRef.ts"), { encoding: "utf-8" });
+    const code = fs.readFileSync(path.join(__dirname, "../output/OnlyLocalRef.ts"), { encoding: "utf-8" });
     expect(code).toMatchSnapshot();
   });
   test("RemoteRef.ts", () => {
-    const code = fs.readFileSync(path.join(__dirname, "../RemoteRef.ts"), { encoding: "utf-8" });
+    const code = fs.readFileSync(path.join(__dirname, "../output/RemoteRef.ts"), { encoding: "utf-8" });
     expect(code).toMatchSnapshot();
   });
 });
